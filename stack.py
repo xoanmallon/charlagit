b7eae7bdce81b239925852a702adee40f26887a9
Xoán Mallón <xoan.mallon@udc.es>
2016-02-10 19:30:51 +0100

    Engadida a pila OO stack.py

---
+++ stack.py
@@ -0,0 +1,21 @@
+# Implementación de unha pila facendo uso de programacion orientada a obxectos.
+class Stack:
+	def __init__(self):
+		self.items = []
+
+	def vacia(self):
+		return self.items == []
+
+	def apilar(self, item):
+		self.items.append(item)
+
+	def cima(self):
+		if self.vacia():
+			return []
+	return self.items[-1]
+
+	def desapilar(self):
+		del self.items[-1]
+
+	def tam(self):
+		return len(self.items)
